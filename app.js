9186d33fa742c870357f9d75a17e7edbb9fcddbe
pkpepe <aunghtetthu.aht98@gmail.com>
2020-02-12 12:10:37 +0630

    just test

--- app.js
+++ app.js
@@ -169,6 +169,32 @@ function handleMessage(sender_psid, received_message) {
     response = {
       "text":'Hello..Min Ga Lar Par Shint. What can I help You?'
     }
+     response = {
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"button",
+        "text":"Try the postback button!",
+        "buttons":[ 
+          {
+            "type":"postback",
+            "title":"Sell my car",
+            "payload":"one"
+          },
+          {
+            "type":"postback",
+            "title":"Find me a car",
+            "payload":"two"
+          },
+          {
+            "type":"postback",
+            "title":"Trend only",
+            "payload":"three"
+          }
+        ]
+      }
+    }
+  }
 
   }
   else if (received_message.text == "button") {
