ac761ba74bd051bc1cd6ac73fff765aa4fb9fb41
pkpepe <aunghtetthu.aht98@gmail.com>
2020-02-12 14:30:09 +0630

    try

--- app.js
+++ app.js
@@ -395,7 +395,7 @@ else if (payload === 'one') {
         "elements":[
            {
             "title":"2013 Honda Fit Shuttle",
-            "image_url":"https://This PC/Desktop/Fit Shuttle.jpg",
+            "image_url":"https://petersfancybrownhats.com/company_image.png",
             "subtitle":"MMK : 250 lkh",
             "default_action": {
               "type": "web_url",
