577c1c47f9e1751cc955fca0cc10d1d0774e36e3
pkpepe <aunghtetthu.aht98@gmail.com>
2020-02-13 11:01:34 +0630

    try

--- app.js
+++ app.js
@@ -656,6 +656,13 @@ function handleMessage(sender_psid, received_message) {
       "text": `Thank you too :)!`
     }
   }
+   else if (received_message.text == "Thank you") {    
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      "text": `Thank you too :)!`
+    }
+  }
    else if (received_message.text) {    
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
