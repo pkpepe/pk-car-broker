d52cf69e3c4eacaf266e396b88a26033a4fe92ad
pkpepe <aunghtetthu.aht98@gmail.com>
2020-05-01 23:14:04 +0630

    renew

--- app.js
+++ app.js
@@ -261,14 +261,6 @@ else if (received_message.text && reqdtp.reqthank == true){
     reqdtp.reqimg = false;
     reqdtp.reqtime = true;
   }
-   else if (received_message.text && reqdtp.reqtime == true){
-    user_say.reqday = received_message.text;
-    response = {
-      "text": "Would you like to leave a phone number?"
-    }
-    reqdtp.reqtime = false;
-  
-  }
   else if (received_message.text == "Hi") {    
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
