3ffce914013630a90ccaeff1373ff324e8ed58b4
pkpepe <aunghtetthu.aht98@gmail.com>
2020-05-01 15:02:59 +0630

    test

--- app.js
+++ app.js
@@ -101,10 +101,7 @@ app.get('/webhook', (req, res) => {
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
 
-  let reqdtp ={
-    reqday:false,
-  };
-  let user_say ={};
+ 
 
   // Check if a token and mode were sent
   if (mode && token) {
@@ -1282,19 +1279,7 @@ function handleMessage(sender_psid, received_message) {
     ]
     }
   }
-  else if (received_message.text == "Date,Time,Ph No") {
-    response = {
-     "text": "Which day do you want to see?"
-    }
-    reqdtp.reqday = true;
-  }
-  else if (received_message.text && reqdtp.reqday = true){
-    user_say.reqday = received_message.text;
-    response = {
-      "text": "Choose Time"
-    }
-    reqdtp.reqday = false;
-  }
+  
   // Send the response message
   callSendAPI(sender_psid, response);   
 }
