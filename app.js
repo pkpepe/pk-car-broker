75f9b5c73b880afac7d5416262a0cfc35a4c17fa
pkpepe <aunghtetthu.aht98@gmail.com>
2020-05-01 12:10:12 +0630

    try

--- app.js
+++ app.js
@@ -120,7 +120,9 @@ app.get('/webhook', (req, res) => {
     }
   }
 });
+var make = {
 
+}
 function handleMessage(sender_psid, received_message) {
   let response;
   
@@ -1279,39 +1281,7 @@ function handleMessage(sender_psid, received_message) {
     ]
     }
   }
-   else if (received_message.text == "Date,Time,Ph No"){
-    response = {
-      "text" : "Date:"
-    }
-   }
-   else if (received_message.text == "2020"){
-    response = {
-      "text" : "Time:"
-    }
-   }
-  else if (received_message.text == "1 pm"){
-    response = {
-      "text" : "Ph No"
-    }
-   }
-   else if (received_message.text == "09"){
-    response = {
-    "attachment":{
-      "type":"template",
-      "payload":{
-        "template_type":"button",
-        "text":"Thank You :). And Where do you want to look? Would you please choice location?",
-        "buttons":[ 
-          {
-            "type":"postback",
-            "title":"Location",
-            "payload":"lc"
-          }
-          ]
-        }
-      }
-    }
-   }
+   
   // Send the response message
   callSendAPI(sender_psid, response);    
 }
